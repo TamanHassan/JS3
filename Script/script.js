a84606966696d69b799c08d70fd46ecc5b53fe95
Taman Hassan <tamanhassan1@gmail.com>
2025-10-20 13:55:56 +0200

    added code to script.js

---
+++ Script/script.js
@@ -0,0 +1,14 @@
+function isValidPassword (password, username) {
+    if (password.length < 8)  {
+        return false;
+        }
+
+    if (password.includes(" ")) {
+        return false;
+        }
+
+    if (password.includes(username)) {
+        return false;
+         }
+         return true;
+          }
